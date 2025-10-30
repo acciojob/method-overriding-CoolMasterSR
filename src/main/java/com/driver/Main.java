51d379161298307ed07227a9fd6888056c95d24d
sumit <sumit.pyrawat@gmail.com>
2025-10-30 12:06:57 +0530

    Overidden method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        A objA = new A();
+        System.out.println(objA.meth());
         B bobj=new B();
        System.out.println(bobj.meth());
     }
