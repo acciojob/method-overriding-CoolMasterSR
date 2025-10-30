053fd80fe7d8fd2775083c27893f0b49aaa4d1ec
sumit <sumit.pyrawat@gmail.com>
2025-10-30 12:01:14 +0530

    New Overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,6 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B bobj=new B();
-        bobj.meth();
+       System.out.println(bobj.meth());
     }
 }
